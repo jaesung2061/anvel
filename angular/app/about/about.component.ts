28911ef957fd169854673fdaf0f2951ee3553567
Jeff Yeon <jeffyeon2061@gmail.com>
2016-05-22 23:25:02 -0700

    Add new "About" page

---
+++ angular/app/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'avl-about',
+    templateUrl: 'app/about/about.component.html',
+    styleUrls: ['app/about/about.component.css']
+})
+export class AboutComponent {
+    constructor() {
+    }
+}
--- angular/app/app.component.ts
+++ angular/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { RouteConfig, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
+import { AboutComponent } from './about';
 import { HomeComponent } from './home';
 import {
     FooterComponent,
@@ -18,7 +19,8 @@ import {
     providers: [ ROUTER_PROVIDERS, HTTP_PROVIDERS, HttpClient ]
 })
 @RouteConfig([
-    new Route('/', 'Home', HomeComponent, {useAsDefault: true})
+    new Route('/', 'Home', HomeComponent, {useAsDefault: true}),
+    new Route('/about', 'About', AboutComponent)
 ])
 export class AppComponent implements OnInit {
     constructor() {
