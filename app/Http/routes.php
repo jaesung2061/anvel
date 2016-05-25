96e36ca2c8b88c6d88ad99f676db74c690b428ec
Jeff Yeon <jeffyeon2061@gmail.com>
2016-05-24 18:17:25 -0700

    Pass APP_ENV to client

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,18 +2,19 @@
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', ['middleware' => 'cors'], function ($api)
-{
+$api->version('v1', ['middleware' => 'cors'], function ($api) {
+    // Authentication routes
     $api->post('auth', 'App\Http\Controllers\AuthController@login');
     $api->get('auth', 'App\Http\Controllers\AuthController@verify');
     $api->delete('auth', 'App\Http\Controllers\AuthController@destroy');
-    
+
     $api->resource('users', 'App\Http\Controllers\UsersController');
 });
 
-$app->get('{slug:.*}', function () use ($app)
-{
+$app->get('{slug:.*}', function () use ($app) {
+    // Careful not to pass secret keys to client here.
     $envVars = json_encode([
+        'APP_ENV'            => env('APP_ENV'),
         'API_STANDARDS_TREE' => env('API_STANDARDS_TREE'),
         'API_SUBTYPE'        => env('API_SUBTYPE'),
         'API_VERSION'        => env('API_VERSION')
