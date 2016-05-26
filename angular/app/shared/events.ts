4e213ef2f7f95ff0d796ac9b75c765db0b26fff8
Jeff Yeon <jeffyeon2061@gmail.com>
2016-05-26 00:56:10 -0700

    Create events constants file

---
+++ angular/app/shared/events.ts
@@ -0,0 +1,9 @@
+export const EVENTS = {
+    AUTH: {
+        LOGIN_SUCCESS: 'auth-login-success',
+        LOGIN_FAILURE: 'auth-login-failed',
+        LOGOUT_SUCCESS: 'auth-logout-success',
+        LOGOUT_FAILURE: 'auth-logout-failure',
+        SESSION_TIMEOUT: 'auth-session-timeout',
+    }
+};
