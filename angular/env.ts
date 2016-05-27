c99547d8df620bdaf598ea17d8bc408f299910b4
Jeff Yeon <jeffyeon2061@gmail.com>
2016-05-26 20:17:56 -0700

    Clean up code

--- angular/env.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Here we bring server-side environment variables
- * into the Angular framwork.
- */
-export const ENV = window['APP_ENVIRONMENT'];
--- angular/polyfills.ts
+++ angular/polyfills.ts
@@ -1,32 +1,14 @@
-// Polyfills
-// (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
-
-// import 'ie-shim'; // Internet Explorer
-// import 'es6-shim';
-// import 'es6-promise';
-// import 'es7-reflect-metadata';
-
-// Prefer CoreJS over the polyfills above
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 require('zone.js/dist/zone');
 
-// Typescript emit helpers polyfill
 import 'ts-helpers';
-
-Error['stackTraceLimit'] = Infinity;
 
 require('zone.js/dist/long-stack-trace-zone');
 
-// if ('production' === ENV) {
-//     //Production
-//
-//
-// } else {
-//     //Development
-//
-//     Error.stackTraceLimit = Infinity;
-//
-//     require('zone.js/dist/long-stack-trace-zone');
-//
-// }
+if (window['APP_ENVIRONMENT'] === 'production') {
+    Error['stackTraceLimit'] = Infinity;
+} else {
+    Error['stackTraceLimit'] = Infinity;
+    require('zone.js/dist/long-stack-trace-zone');
+}
