d9dc27017e08c1667c7f975716cec8eeb9890fcd
Jeff Yeon <jeffyeon2061@gmail.com>
2016-05-30 04:22:37 -0700

    Update old code

--- angular/app/pages/home/home.component.ts
+++ angular/app/pages/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
 
-import { Api } from '../../services/api/api.service';
+import { Auth } from '../../services/auth/auth.service';
 
 @Component({
     selector: 'avl-home',
@@ -16,33 +14,15 @@ export class HomeComponent {
         password: ''
     };
 
-    constructor(private api: Api) {
+    constructor(private auth: Auth) {
     }
 
     login() {
-        let data = JSON.stringify(this.credentials);
-
-        this.api.post('auth', data)
-            .map(this.extractData)
-            .catch(this.handleError)
+        this.auth.login(this.credentials)
             .subscribe((response: any) => {
                 console.log(response);
             }, (test: any) => {
                 console.log(test);
             });
-    }
-
-    private extractData(res: Response) {
-        let body = res.json();
-        return body.data || { };
-    }
-
-    private handleError (error: any) {
-        // In a real world app, we might use a remote logging infrastructure
-        // We'd also dig deeper into the error to get a better message
-        let errMsg = (error.message) ? error.message :
-            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-        console.error(errMsg); // log to console instead
-        return Observable.throw(errMsg);
     }
 }
