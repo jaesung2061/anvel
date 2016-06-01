7104221420eeef31077122b0296f85dad812a64d
Jeff Yeon <jeffyeon2061@gmail.com>
2016-06-01 00:04:47 -0700

    Comment out unused route and leave as example

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,12 +3,20 @@
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', ['middleware' => 'cors'], function ($api) {
+    // All routes in this callback is prefixed by "/api"
+    // To change this, go to .env
+
     // Authentication routes
     $api->post('auth', 'App\Http\Controllers\AuthController@login');
     $api->get('auth', 'App\Http\Controllers\AuthController@verify');
     $api->delete('auth', 'App\Http\Controllers\AuthController@destroy');
 
-    $api->resource('users', 'App\Http\Controllers\UsersController');
+    // The following command will create these routes.
+    //   - GET    /api/users
+    //   - POST   /api/users
+    //   - PATCH  /api/users/{id}
+    //   - DELETE /api/users/{id}
+    // $api->resource('users', 'App\Http\Controllers\UsersController');
 });
 
 $app->get('{slug:.*}', 'AngularController@serve');
