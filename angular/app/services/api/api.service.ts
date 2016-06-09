1be6852b41c2858eb91074ce00a7ee63882a1773
Jeff Yeon <jeffyeon2061@gmail.com>
2016-06-08 20:04:20 -0700

    don't return object

--- angular/app/services/api/api.service.ts
+++ angular/app/services/api/api.service.ts
@@ -216,7 +216,7 @@ export class Api {
     private extractData(response: any): any {
         let body = response.json();
 
-        return body.data || {};
+        return body.data;
     }
 
     /**
