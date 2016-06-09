366403aaece29283e13090b89c08ded88a2be735
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2016-06-09 15:18:29 +0545

    Sub Page Create Artisan Command

---
+++ app/Console/Commands/AngularGenerators/CreateSubPageCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands\AngularGenerators;
+
+class CreateSubPageCommand extends BaseGeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ng:subpage {name} {pageName} {--path=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create component files for Angular 2 Sub Page.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->validateInput();
+
+           /**
+     * Name of Subpage Component will be {pageName.name}  .. to prevent name collide.
+     */ 
+
+        $componentName = $this->argument('pageName').$this->argument('name');
+
+          /**
+     * Target of Subpage Component will be pages/{pageName}/subpages/{pageName.name} 
+     */ 
+        $targetDir = $this->getTargetDir('pages',$this->argument('pageName').'/subpages', $componentName);
+            
+        $type = 'component';
+
+        $this->createTs($componentName, $type, $targetDir.$componentName.'.component.ts');
+        $this->createHtml($componentName, $type, $targetDir.$componentName.'.component.html');
+        $this->createScss($componentName, $type, $targetDir.$componentName.'.component.scss');
+        $this->createSpec($componentName, $type, $targetDir.$componentName.'.spec.ts');
+        $this->createIndex($componentName, $type, $targetDir.'index.ts');
+    }
+}
