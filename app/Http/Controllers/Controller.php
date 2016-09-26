04f9d540e4c89ffebbdc027d5e15e031af5dce42
Jeff Yeon <jeffyeon2061@gmail.com>
2016-09-25 20:00:15 -0700

    Add helper method for easy responses to the Angular client

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,17 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * Simple wrapper that wraps responses into a
+     * format that the Angular client expects.
+     *
+     * @param  string  $content
+     * @param  int     $status
+     * @param  array   $headers
+     * @return \Illuminate\Http\Response|\Laravel\Lumen\Http\ResponseFactory
+     */
+    public function api($content = '', $status = 200, array $headers = [])
+    {
+        return response(['data' => $content], $status, $headers);
+    }
 }
