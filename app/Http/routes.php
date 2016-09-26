1c480b722d751f4c7b7568f61905e08865a4df11
Jeff Yeon <jeffyeon2061@gmail.com>
2016-09-25 20:02:13 -0700

    Namespace route group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,21 +3,21 @@
 /* @var $api Dingo\Api\Routing\Router */
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', ['middleware' => 'cors'], function (Dingo\Api\Routing\Router $api) {
+$api->version('v1', ['middleware' => 'cors', 'namespace' => 'App\Http\Controllers'], function (Dingo\Api\Routing\Router $api) {
     // All routes in this callback is prefixed by "/api"
     // To change this, go to .env
 
     // Authentication routes
-    $api->post('auth', 'App\Http\Controllers\AuthController@login');
-    $api->get('auth', 'App\Http\Controllers\AuthController@verify');
-    $api->delete('auth', 'App\Http\Controllers\AuthController@destroy');
+    $api->post('auth', 'AuthController@login');
+    $api->get('auth', 'AuthController@verify');
+    $api->delete('auth', 'AuthController@destroy');
 
     // The following call will create these routes.
     //   - GET    /api/users
     //   - POST   /api/users
     //   - PATCH  /api/users/{id}
     //   - DELETE /api/users/{id}
-    // $api->resource('users', 'App\Http\Controllers\UsersController');
+    // $api->resource('users', 'UsersController');
 });
 
 $app->get('{slug:.*}', 'AngularController@serve');
