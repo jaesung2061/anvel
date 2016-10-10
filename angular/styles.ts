a09ad53e9e5c7b91d101634d61655da861fcef3d
Jeff Yeon <jeff@jeffs-imac.attlocal.net>
2016-10-09 23:49:15 -0700

    Refactor styles and move Bootstrap to source directory

--- angular/styles.ts
+++ angular/styles.ts
@@ -1,4 +1,6 @@
+import '!style!css!sass!./styles/bootstrap/bootstrap-flex.scss';
 
-import '!style!css!sass!bootstrap/scss/bootstrap-flex.scss';
-import '!style!css!sass!loaders.css/src/loaders.scss';
-import '!style!css!sass!./globals.scss';
+import '!style!css!sass!./styles/globals.scss';
+import '!style!css!sass!./styles/layout.scss';
+import '!style!css!sass!./styles/utilities.scss';
+import '!style!css!sass!./styles/variables.scss';
