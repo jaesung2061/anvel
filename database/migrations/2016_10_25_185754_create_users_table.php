37f32f0f2f44375698df5ac1ea85c3c01bc558d4
Jeff Yeon <jeffyeon2061@gmail.com>
2016-10-25 12:49:41 -0700

    Add default migration for users table

---
+++ database/migrations/2016_10_25_185754_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->timestamps();
+        });
+
+        User::create([
+            'email'    => 'test@test.com',
+            'password' => '123123'
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
