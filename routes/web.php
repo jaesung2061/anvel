75a3d2eef79e6287e63e8160d4fb7dc6314b3837
Jeff Yeon <jeffyeon2061@gmail.com>
2017-04-25 21:59:46 -0700

    remove boilerplate

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,6 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', ['namespace' => 'App\Http\Controllers'], function (Dingo\Api\Routing\Router $api) {
     // All routes in this callback is prefixed by "/api"
-    // To change this, go to .env
-    // Authentication routes
-    $api->post('auth', 'AuthController@login');
-    $api->get('auth', 'AuthController@verify');
-    $api->delete('auth', 'AuthController@destroy');
-    // The following call will create these routes.
-    //   - GET    /api/users
-    //   - POST   /api/users
-    //   - PATCH  /api/users/{id}
-    //   - DELETE /api/users/{id}
-    // $api->resource('users', 'UsersController');
 });
 
 $app->get('/', 'AngularController@serve');
