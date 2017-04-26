875c0fffba7bcb331fd7253b08e5fb623485da3c
Jeff Yeon <jeffyeon2061@gmail.com>
2017-04-25 22:22:48 -0700

    add angular material

--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -1,4 +1,6 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -10,9 +12,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule,
+    MdCheckboxModule
+  ],
+  exports: [
+    MdButtonModule,
+    MdCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
