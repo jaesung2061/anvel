4e3e2aad27d51cfea53fdf6efadcb4320436b874
Jeff Yeon <jeffyeon2061@gmail.com>
2017-04-27 12:43:26 -0700

    fix bug

--- app/Http/Controllers/AngularController.php
+++ app/Http/Controllers/AngularController.php
@@ -19,6 +19,6 @@ class AngularController extends Controller
 //            'API_VERSION'        => env('API_VERSION')
 //        ]);
 
-        return file_get_contents(public_path('dist/index.html'));
+        return file_get_contents(base_path('public/dist/index.html'));
     }
 }
