f9a125fe51c873bd784596c8c0821b0bd0674a5c
Jeff Yeon <jeffyeon2061@gmail.com>
2017-04-27 12:44:03 -0700

    remove commented code

--- app/Http/Controllers/AngularController.php
+++ app/Http/Controllers/AngularController.php
@@ -11,14 +11,6 @@ class AngularController extends Controller
      */
     public function serve()
     {
-//        // Careful not to pass secret keys to client.
-//        $envVars = json_encode([
-//            'APP_ENV'            => env('APP_ENV'),
-//            'API_STANDARDS_TREE' => env('API_STANDARDS_TREE'),
-//            'API_SUBTYPE'        => env('API_SUBTYPE'),
-//            'API_VERSION'        => env('API_VERSION')
-//        ]);
-
         return file_get_contents(base_path('public/dist/index.html'));
     }
 }
