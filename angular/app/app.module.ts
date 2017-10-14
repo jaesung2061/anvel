c7e0f6e7845928cb0db4ade335d18fade50f642e
Kenneth Sinder <kennethsinder@gmail.com>
2017-10-14 14:26:37 -0400

    Update to Laravel 5.5 and PHP7 (#33)
    * Use Laravel 5.5 and PHP 7
    
    * Add composer lock to repo
    
    * Allow newer versions of angular cli
    
    * Use new Material module syntax
    
    * Add page load sanity test to php feature test
    
    * Bump material version

--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -16,12 +16,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MdButtonModule,
-    MdCheckboxModule
+    MatButtonModule,
+    MatCheckboxModule
   ],
   exports: [
-    MdButtonModule,
-    MdCheckboxModule
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
