82c2a795e2ad413ddc72195f0267c66ea8612d85
Kenneth Sinder <kennethsinder@gmail.com>
2017-10-13 18:41:20 -0700

    Add page load sanity test to php feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,6 +18,7 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+	$response->assertStatus(200);
+	$response->assertSee('app-root');
     }
 }
